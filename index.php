998c1af1e4d8f2c55932da8186a48fbf7ca69a51
Richiisontheway <mineiriccardo@gmail.com>
2024-05-24 01:27:13 +0200

    end Book classes

--- index.php
+++ index.php
@@ -36,5 +36,54 @@
         public $author;
         public $genre;
         public $published_year;
-        
-    }
+        public function __construct(string $libro,string $autore,string $genere, $anno_di_pubblicazione)
+        {
+            $this->book = $libro;
+            $this->author = $autore;
+            $this->genre = $genere;
+            $this->published_year = $anno_di_pubblicazione; 
+        }
+        public function getBook(){
+            return $this->book;
+        }
+        public function setBook($book){
+            if(is_string($this->book)){
+                return $this->book = $book;
+            }else{
+                throw new Exception("Error Book is not a string");
+            }
+        }
+        public function getAuthor(){
+            return $this->book;
+        }
+        public function setAuthor($author){
+            if(is_string($this->author)){
+                return $this->author = $author;
+            }else{
+                throw new Exception("Error Author is not a string");
+                
+            }
+        }
+        public function getGenre(){
+            return $this->genre;
+        }
+        public function setGenre($genre){
+            if(is_string($this->genre)){
+                return $this->genre = $genre;
+            }else{
+                throw new Exception("Error Genre is not a string");
+            }
+        }
+        public function getPublishedYear(){
+            return $this->published_year;
+        }
+        public function setPublishedYear($year){
+            return $this->published_year = $year;
+        }
+        public function getInfo(){
+            return " il nome del libro è $this->book, di $this->author pubblicato il $this->published_year e fa parte del genere $this->genre";
+        }
+    };
+
+    $book = new Book('Harry Potter', 'Jk Rowling','fantasy', 2014);
+    echo $book->getInfo();
